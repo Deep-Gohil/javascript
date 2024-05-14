7a676307c921686c4eebd7b482639012405d483c
Deep-Gohil <149695045+Deep-Gohil@users.noreply.github.com>
2024-05-14 08:43:58 +0530

    Add files via upload

--- Lecture-18/script.js
+++ Lecture-18/script.js
@@ -1,21 +1,21 @@
-let student = []
+let student = [];
+
 const handleStudentData = (e) => {
     e.preventDefault();
     let data = {
-            name : document.getElementById("name").value,
-            course : document.getElementById("course").value,
-            grid : document.getElementById("grid").value,
-        }
-        // console.log(data.name, data.course, data.grid);
-        student.push(data);
-        UIMaker();
-
+        name: document.getElementById("name").value,
+        course: document.getElementById("course").value,
+        grid: document.getElementById("grid").value,
+    }
+    student.push(data);
+    UIMaker();
 }
+
 document.getElementById("studentData").addEventListener("submit", handleStudentData);
-const UIMaker = () =>{
-    // document.getElementById("studentList").innerHTML = "";
-    document.getElementById("tbody-new").innerHTML = "";
-    student.map((ele)=>{
+
+const UIMaker = () => {
+    document.getElementById("tbody").innerHTML = "";
+    student.map((ele, index) => {
         let tr = document.createElement("tr");
         let td1 = document.createElement("td");
         td1.innerHTML = ele.name;
@@ -23,12 +23,23 @@ const UIMaker = () =>{
         td2.innerHTML = ele.course;
         let td3 = document.createElement("td");
         td3.innerHTML = ele.grid;
+        let deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete";
+        deleteButton.addEventListener("click", () => handleDelete(index));
+        let td4 = document.createElement("td");
+        td4.appendChild(deleteButton);
+        tr.append(td1, td2, td3, td4);
+        document.getElementById("tbody").append(tr);
+    });
+}
 
-        tr.append(td1,td2,td3);
-        document.getElementById("studentList").append(tr);
-    })
-}
-const handleDelete = () =>{
+const deleteAll = () =>{
     document.getElementById("tbody").innerHTML = "";
+    student = [];
 }
-document.getElementById("deleteAllhh").addEventListener("click", handleDelete);
+
+const handleDelete = (index) => {
+    student.splice(index, 1);
+    UIMaker();
+}
+document.getElementById("deleteAll").addEventListener("click",deleteAll)
