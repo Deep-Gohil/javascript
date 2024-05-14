8e5c6e43f09b02cbcc859ff7d31d8659d5c871d2
Deep-Gohil <149695045+Deep-Gohil@users.noreply.github.com>
2024-05-14 11:39:34 +0530

    Add files via upload

---
+++ Exam-1/script.js
@@ -0,0 +1,77 @@
+let company = [];
+
+let count = 1;
+const UIMaker = () => {
+    document.getElementById("tbody").innerHTML = "";
+    company.map((ele, index) => {
+
+        tr = document.createElement("tr");
+        employee = document.createElement("td");
+        employee.innerHTML = ele.name;
+        job = document.createElement("td");
+        job.innerHTML = ele.job;
+        department = document.createElement("td");
+        department.innerHTML = ele.company;
+        salary = document.createElement("td");
+        salary.innerHTML = ele.salary;
+        email = document.createElement("td");
+        email.innerHTML = ele.email;
+        experience = document.createElement("td");
+        experience.innerHTML = ele.experience;
+        role = document.createElement("td");
+        deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete";
+        deleteButton.addEventListener("click", () => handleDelete(index));
+        deleteButton.addEventListener("click", countMin);
+
+        
+
+        td4 = document.createElement("td");
+        td4.append(deleteButton);
+        if (ele.experience > 5) {
+            role.innerHTML = "Senior";
+        }
+        else {
+            role.innerHTML = "junior";
+        }
+        countNew = document.createElement("td");
+        countNew.innerHTML = count;
+        tr.append(employee, job, department, salary, email, experience, role, td4,)
+        document.getElementById("tbody").append(tr);
+    });
+    document.getElementById("result").innerHTML = `Total Employee ${count}`;
+}
+const handlCompanyData = (e) => {
+    e.preventDefault()
+    let data = {
+        name: document.getElementById("name").value,
+        job: document.getElementById("job role").value,
+        company: document.getElementById("department").value,
+        salary: document.getElementById("salary").value,
+        experience: document.getElementById("experience").value,
+        email: document.getElementById("contact").value,
+    }
+    console.log(data);
+    company.push(data);
+    UIMaker();
+}
+const handleDelete = (index) => {
+    company.splice(index, 1);
+    UIMaker();
+    count--;
+}
+const deleteAll = () => {
+    document.getElementById("tbody").innerHTML = "";
+    company = [];
+    count = 0;
+}
+const countTotal = () => {
+    count ++;
+}
+const countMin = () => {
+    count --;
+    countTotal();
+}
+document.getElementById("fire").addEventListener("click", deleteAll)
+document.getElementById("hire").addEventListener("click", handlCompanyData)
+document.getElementById("hire").addEventListener("click", countTotal)
